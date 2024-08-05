5b7811e4f482bdfea19f4770465ea6264449767e
ayush singh <ayushbaragaon20@gmail.com>
2024-08-05 17:45:59 +0530

    drawer

--- catalog_app/lib/widgets/drawer.dart
+++ catalog_app/lib/widgets/drawer.dart
@@ -5,7 +5,13 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final imageUrl =
-        "https://preview.keenthemes.com/metronic-v4/theme_rtl/assets/pages/media/profile/profile_user.jpg";
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5dJ3ih4pbDyYTj9I5PlwnG5JlETsaSgeskXCW7wbBanLmklbqwfPFinNMpYOyITO1H4M&usqp=CAU";
+    //"https://i.ytimg.com/vi/hK6Cx04ipnU/sddefault.jpg";
+    //  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_q2Td5le6Ex47LKyp-JnY6uysDsY0R8ou9PNiqvQ63rA0ROw70oblOPUeohr6QfTA4U4&usqp=CAU';
+    //"https://media.tenor.com/BppqBqkdG5EAAAAM/koga-sum-te-pital.gif";
+    // 'https://9meters.com/wp-content/uploads/funny-ai-generated-profile-pics.webp';
+    // "https://static.toiimg.com/thumb/msid-112284251,width-1070,height-580,imgsize-187441,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg";
+    //"https://preview.keenthemes.com/metronic-v4/theme_rtl/assets/pages/media/profile/profile_user.jpg";
     return Drawer(
       child: Container(
         color: Colors.blueGrey,
@@ -17,11 +23,16 @@ class MyDrawer extends StatelessWidget {
               child: UserAccountsDrawerHeader(
                 margin: EdgeInsets.zero,
                 decoration: BoxDecoration(color: Colors.blueGrey),
-                accountName: Text('Ayush Singh'),
-                accountEmail: Text('ayush@gmail.com'),
+                accountName: Text('Dam Dam'),
+                accountEmail: Text('chintapak@gmail.com'),
                 currentAccountPicture: //Image.network(imageUrl),
-                    CircleAvatar(
-                  backgroundImage: NetworkImage(imageUrl),
+                    SizedBox(
+                  height: 900,
+                  width: 900,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(imageUrl),
+                  ),
                 ),
               ),
             ),
